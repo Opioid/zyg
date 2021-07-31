f0b90ba79ccdcb64e0e60cdc6cfd748d111e063a
Opioid <benjamin.savs@gmail.com>
2021-07-31 09:53:20 +0200

    PNG writer assumes input in sRGB color space now

--- src/core/image/encoding/srgb.zig
+++ src/core/image/encoding/srgb.zig
@@ -1,4 +1,5 @@
 const Float4 = @import("../../image/image.zig").Float4;
+usingnamespace @import("base");
 
 const Allocator = @import("std").mem.Allocator;
 
@@ -14,8 +15,8 @@ pub const Srgb = struct {
     }
 
     pub fn resize(self: *Srgb, alloc: *Allocator, num_pixels: u32) !void {
-        const num_channeles: u32 = if (self.alpha) 4 else 3;
-        const num_bytes = num_pixels * num_channeles;
+        const num_channels: u32 = if (self.alpha) 4 else 3;
+        const num_bytes = num_pixels * num_channels;
 
         if (num_bytes > self.buffer.len) {
             alloc.free(self.buffer);
@@ -27,16 +28,16 @@ pub const Srgb = struct {
     pub fn toSrgb(self: *Srgb, image: Float4) void {
         if (self.alpha) {
             for (image.pixels) |p, i| {
-                self.buffer[i * 4 + 0] = @floatToInt(u8, 255.0 * std.math.min(p.v[0], 1.0));
-                self.buffer[i * 4 + 1] = @floatToInt(u8, 255.0 * std.math.min(p.v[1], 1.0));
-                self.buffer[i * 4 + 2] = @floatToInt(u8, 255.0 * std.math.min(p.v[2], 1.0));
-                self.buffer[i * 4 + 3] = @floatToInt(u8, 255.0 * std.math.min(p.v[3], 1.0));
+                self.buffer[i * 4 + 0] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[0]));
+                self.buffer[i * 4 + 1] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[1]));
+                self.buffer[i * 4 + 2] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[2]));
+                self.buffer[i * 4 + 3] = encoding.floatToUnorm(std.math.min(p.v[3], 1.0));
             }
         } else {
             for (image.pixels) |p, i| {
-                self.buffer[i * 3 + 0] = @floatToInt(u8, 255.0 * std.math.min(p.v[0], 1.0));
-                self.buffer[i * 3 + 1] = @floatToInt(u8, 255.0 * std.math.min(p.v[1], 1.0));
-                self.buffer[i * 3 + 2] = @floatToInt(u8, 255.0 * std.math.min(p.v[2], 1.0));
+                self.buffer[i * 3 + 0] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[0]));
+                self.buffer[i * 3 + 1] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[1]));
+                self.buffer[i * 3 + 2] = encoding.floatToUnorm(spectrum.linearToGamma_sRGB(p.v[2]));
             }
         }
     }
