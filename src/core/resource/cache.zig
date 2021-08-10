5b754704e21e74161cf44d1dde0823be0037ed27
Opioid <benjamin.savs@gmail.com>
2021-08-10 23:26:40 +0200

    Resource cache does some caching

--- src/core/resource/cache.zig
+++ src/core/resource/cache.zig
@@ -8,17 +8,22 @@ pub const Null = 0xFFFFFFFF;
 pub fn Cache(comptime T: type, comptime P: type) type {
     return struct {
         provider: P,
-
         resources: std.ArrayListUnmanaged(T),
+        entries: std.StringHashMap(u32),
 
         const Self = @This();
 
         pub fn init(alloc: *Allocator, provider: P) Self {
-            _ = alloc;
-            return .{ .provider = provider, .resources = std.ArrayListUnmanaged(T){} };
+            return .{
+                .provider = provider,
+                .resources = std.ArrayListUnmanaged(T){},
+                .entries = std.StringHashMap(u32).init(alloc),
+            };
         }
 
         pub fn deinit(self: *Self, alloc: *Allocator) void {
+            self.entries.deinit();
+
             for (self.resources.items) |*r| {
                 r.deinit(alloc);
             }
@@ -27,11 +32,19 @@ pub fn Cache(comptime T: type, comptime P: type) type {
         }
 
         pub fn load(self: *Self, alloc: *Allocator, name: []const u8, resources: *Resources) !u32 {
+            if (self.entries.get(name)) |entry| {
+                return entry;
+            }
+
             const item = try self.provider.load(alloc, name, resources);
 
             try self.resources.append(alloc, item);
 
-            return @intCast(u32, self.resources.items.len - 1);
+            const id = @intCast(u32, self.resources.items.len - 1);
+
+            try self.entries.put(name, id);
+
+            return id;
         }
 
         pub fn store(self: *Self, alloc: *Allocator, item: T) u32 {
