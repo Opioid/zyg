5bed2efa1940b38f3d3cf5ef144650d071ce46e6
Opioid <benjamin.savs@gmail.com>
2021-09-17 08:05:47 +0200

    SRGB_FLOAT table can be evaluated now

--- src/core/image/texture/encoding.zig
+++ src/core/image/texture/encoding.zig
@@ -12,13 +12,7 @@ const UNORM_FLOAT = calculateUnormToFloat();
 const SNORM_FLOAT = calculateSnormToFloat();
 
 pub fn cachedSrgbToFloat3(byte: Vec3b) Vec4f {
-    //  return .{ SRGB_FLOAT[byte.v[0]], SRGB_FLOAT[byte.v[1]], SRGB_FLOAT[byte.v[2]], 0.0 };
-    return .{
-        spectrum.gammaToLinear_sRGB(@intToFloat(f32, byte.v[0]) * (1.0 / 255.0)),
-        spectrum.gammaToLinear_sRGB(@intToFloat(f32, byte.v[1]) * (1.0 / 255.0)),
-        spectrum.gammaToLinear_sRGB(@intToFloat(f32, byte.v[2]) * (1.0 / 255.0)),
-        0.0,
-    };
+    return .{ SRGB_FLOAT[byte.v[0]], SRGB_FLOAT[byte.v[1]], SRGB_FLOAT[byte.v[2]], 0.0 };
 }
 
 pub fn cachedUnormToFloat(byte: u8) f32 {
@@ -32,11 +26,13 @@ pub fn cachedSnormToFloat2(byte: Vec2b) Vec2f {
 const Num_samples: u32 = 256;
 
 fn calculateSrgbToFloat() [Num_samples]f32 {
+    @setEvalBranchQuota(11000);
+
     var buf: [Num_samples]f32 = undefined;
 
     var i: u32 = 0;
     while (i < Num_samples) : (i += 1) {
-        buf[i] = spectrum.gammaToLinear_sRGB(@intToFloat(f32, i) * (1.0 / 255.0));
+        buf[i] = spectrum.gammaToLinear_sRGB(@intToFloat(f32, i) / 255.0);
     }
 
     return buf;
