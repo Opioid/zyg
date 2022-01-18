5d6ede4ffe8ef79b009d46cf05047ed687992faf
Opioid <benjamin.savs@gmail.com>
2022-01-18 21:29:53 +0100

    Starting to use simple log interface

--- src/core/core.zig
+++ src/core/core.zig
@@ -1,6 +1,7 @@
 pub const camera = @import("camera/perspective.zig");
 pub const file = @import("file/read_stream.zig");
 pub const image = @import("image/image.zig");
+pub const log = @import("log/log.zig");
 pub const rendering = @import("rendering/driver.zig");
 pub const resource = @import("resource/manager.zig");
 pub const sampler = @import("sampler/sampler.zig");
