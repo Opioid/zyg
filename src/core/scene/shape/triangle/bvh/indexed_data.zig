824d320d8bc1da2e0152a52539f6a2ce16dda890
Opioid <benjamin.savs@gmail.com>
2022-04-25 10:15:07 +0200

    Fixed a problem with SUB mesh loading

--- src/core/scene/shape/triangle/bvh/indexed_data.zig
+++ src/core/scene/shape/triangle/bvh/indexed_data.zig
@@ -124,9 +124,9 @@ pub const Indexed_data = struct {
     pub fn interpolateData(self: Self, u: f32, v: f32, index: u32, t: *Vec4f, n: *Vec4f, uv: *Vec2f) void {
         const tri = self.triangles[index];
 
-        const tna = quaternion.initTN(self.frames[tri.a]);
-        const tnb = quaternion.initTN(self.frames[tri.b]);
-        const tnc = quaternion.initTN(self.frames[tri.c]);
+        const tna = quaternion.toTN(self.frames[tri.a]);
+        const tnb = quaternion.toTN(self.frames[tri.b]);
+        const tnc = quaternion.toTN(self.frames[tri.c]);
 
         t.* = math.normalize3(triangle.interpolate3(tna[0], tnb[0], tnc[0], u, v));
         n.* = math.normalize3(triangle.interpolate3(tna[1], tnb[1], tnc[1], u, v));
@@ -167,9 +167,9 @@ pub const Indexed_data = struct {
     pub fn interpolateShadingNormal(self: Self, u: f32, v: f32, index: u32) Vec4f {
         const tri = self.triangles[index];
 
-        const a = quaternion.initNormal(self.frames[tri.a]);
-        const b = quaternion.initNormal(self.frames[tri.b]);
-        const c = quaternion.initNormal(self.frames[tri.c]);
+        const a = quaternion.toNormal(self.frames[tri.a]);
+        const b = quaternion.toNormal(self.frames[tri.b]);
+        const c = quaternion.toNormal(self.frames[tri.c]);
 
         return math.normalize3(triangle.interpolate3(a, b, c, u, v));
     }
