bff0747f79a05d6a797d8fb32dbc26c5504154ed
Opioid <benjamin.savs@gmail.com>
2022-07-05 14:14:23 +0200

    Removed unused function

--- src/core/rendering/sensor/sensor.zig
+++ src/core/rendering/sensor/sensor.zig
@@ -120,17 +120,6 @@ pub const Sensor = union(enum) {
         }
     }
 
-    pub fn mean(self: Sensor, pixel: Vec2i) Vec4f {
-        return switch (self) {
-            .Unfiltered_opaque => |s| s.sensor.mean(pixel),
-            .Unfiltered_transparent => |s| s.sensor.mean(pixel),
-            .Filtered_1p0_opaque => |s| s.sensor.mean(pixel),
-            .Filtered_2p0_opaque => |s| s.sensor.mean(pixel),
-            .Filtered_1p0_transparent => |s| s.sensor.mean(pixel),
-            .Filtered_2p0_transparent => |s| s.sensor.mean(pixel),
-        };
-    }
-
     pub fn addSample(
         self: *Sensor,
         sample: Sample,
