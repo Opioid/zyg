5d4b6651e2598a0f9718b18f5a5407dc90ea8417
Opioid <benjamin.savs@gmail.com>
2023-07-20 22:02:46 +0200

    Just save

--- src/core/scene/shape/curve/curve_mesh.zig
+++ src/core/scene/shape/curve/curve_mesh.zig
@@ -1,1 +1,7 @@
-pub const Mesh = struct {};
+const Trafo = @import("../../composed_transformation.zig").ComposedTransformation;
+
+pub const Mesh = struct {
+    pub fn intersect(self: Mesh, ray: *Ray, trafo: Trafo, isec: *Intersection) bool {
+        const tray = trafo.worldToObjectRay(ray.*);
+    }
+};
