56f152906b1fe8be4b349b01754a5ec944a89c19
Opioid <benjamin.savs@gmail.com>
2023-10-13 21:36:37 +0200

    Vectorized offsetRay() at least reduces code size

--- src/core/scene/ray_offset.zig
+++ src/core/scene/ray_offset.zig
@@ -1,4 +1,5 @@
 const math = @import("base").math;
+const Vec4i = math.Vec4i;
 const Vec4f = math.Vec4f;
 
 pub const Ray_max_t = 3.4027715434167032e+38;
@@ -12,18 +13,18 @@ const float_scale: f32 = 1.0 / 65536.0;
 const int_scale: f32 = 256.0;
 
 pub fn offsetRay(p: Vec4f, n: Vec4f) Vec4f {
-    const of_i = math.vec4fTo4i(@as(Vec4f, @splat(int_scale)) * Vec4f{ n[0], n[1], n[2], 0.0 });
+    const of_i = math.vec4fTo4i(@as(Vec4f, @splat(int_scale)) * n);
 
-    const p_i0 = @as(f32, @bitCast(@as(i32, @bitCast(p[0])) + (if (p[0] < 0.0) -of_i[0] else of_i[0])));
-    const p_i1 = @as(f32, @bitCast(@as(i32, @bitCast(p[1])) + (if (p[1] < 0.0) -of_i[1] else of_i[1])));
-    const p_i2 = @as(f32, @bitCast(@as(i32, @bitCast(p[2])) + (if (p[2] < 0.0) -of_i[2] else of_i[2])));
+    const p_ii: Vec4i = @bitCast(p);
+    const p_in: Vec4f = @bitCast(p_ii - of_i);
+    const p_ip: Vec4f = @bitCast(p_ii + of_i);
+    const p_i = @select(f32, p < @as(Vec4f, @splat(0.0)), p_in, p_ip);
 
-    return .{
-        if (@abs(p[0]) < origin) @mulAdd(f32, float_scale, n[0], p[0]) else p_i0,
-        if (@abs(p[1]) < origin) @mulAdd(f32, float_scale, n[1], p[1]) else p_i1,
-        if (@abs(p[2]) < origin) @mulAdd(f32, float_scale, n[2], p[2]) else p_i2,
-        0.0,
-    };
+    const mad = @mulAdd(Vec4f, @splat(float_scale), n, p);
+
+    const r = @select(f32, @abs(p) < @as(Vec4f, @splat(origin)), mad, p_i);
+
+    return .{ r[0], r[1], r[2], 0.0 };
 }
 
 pub fn offsetF(t: f32) f32 {
