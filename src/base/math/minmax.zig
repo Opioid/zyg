44453ebf7ccb0d90ad4067a8dd940faffcaa4980
Opioid <benjamin.savs@gmail.com>
2024-08-28 10:17:11 -0700

    Generate more efficient min/max code on aarch64

--- src/base/math/minmax.zig
+++ src/base/math/minmax.zig
@@ -1,9 +1,17 @@
+const builtin = @import("builtin");
+
 pub inline fn min(x: f32, y: f32) f32 {
-    return if (x < y) x else y;
+    return switch (builtin.target.cpu.arch) {
+        .aarch64, .aarch64_be => @min(x, y),
+        inline else => if (x < y) x else y,
+    };
 }
 
 pub inline fn max(x: f32, y: f32) f32 {
-    return if (y < x) x else y;
+    return switch (builtin.target.cpu.arch) {
+        .aarch64, .aarch64_be => @max(x, y),
+        inline else => if (y < x) x else y,
+    };
 }
 
 pub inline fn clamp(x: f32, mi: f32, ma: f32) f32 {
