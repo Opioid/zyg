6c819c7190a68c1588a3ba1a78cb3ad45cd69a73
Opioid <benjamin.savs@gmail.com>
2024-09-23 10:15:26 -0700

    Enable hair reader

--- src/core/scene/shape/curve/hair_reader.zig
+++ src/core/scene/shape/curve/hair_reader.zig
@@ -34,7 +34,7 @@ pub const Reader = struct {
     };
 
     pub fn read(alloc: Allocator, stream: ReadStream) !Result {
-        const generic = true;
+        const generic = false;
         if (generic) {
             return try genericCrap(alloc);
         }
