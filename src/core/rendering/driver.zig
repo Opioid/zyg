ed26298f4276567d633df9f7d9d9a47b227e9186
Opioid <benjamin.savs@gmail.com>
2025-01-21 20:20:47 -0800

    Don't know if using optional pointer is really better here

--- src/core/rendering/driver.zig
+++ src/core/rendering/driver.zig
@@ -108,7 +108,7 @@ pub const Driver = struct {
                 view.samplers,
                 view.aovs,
                 view.photon_settings,
-                &self.photon_map,
+                if (self.photon_map.num_paths > 0) &self.photon_map else null,
             );
         }
     }
