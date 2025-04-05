940c41ece66fb15b6ad217207a582cf7dcc7ae0b
Opioid <benjamin.savs@gmail.com>
2025-04-04 23:19:30 -0700

    Fixed what seems likely to be a problem with triplanar mapping

--- src/core/scene/material/material_helper.zig
+++ src/core/scene/material/material_helper.zig
@@ -57,13 +57,13 @@ pub fn sampleNormal(wo: Vec4f, rs: Renderstate, map: Texture, key: ts.Key, sampl
 pub fn triplanarMapping(p: Vec4f, n: Vec4f) Vec2f {
     const an = @abs(n);
     if (an[0] > an[1] and an[0] > an[2]) {
-        const sign = std.math.copysign(@as(f32, 1.0), p[0]);
-        return .{ math.frac(sign * p[1] + 0.5), math.frac(p[2] + 0.5) };
+        const sign = std.math.copysign(@as(f32, 1.0), n[0]);
+        return .{ math.frac(sign * p[2] + 0.5), math.frac(-p[1] + 0.5) };
     } else if (an[1] > an[0] and an[1] > an[2]) {
-        const sign = std.math.copysign(@as(f32, 1.0), p[1]);
-        return .{ math.frac(-sign * p[0] + 0.5), math.frac(p[2] + 0.5) };
+        const sign = std.math.copysign(@as(f32, 1.0), n[1]);
+        return .{ math.frac(sign * p[0] + 0.5), math.frac(-p[2] + 0.5) };
     } else {
-        const sign = std.math.copysign(@as(f32, 1.0), p[2]);
-        return .{ math.frac(sign * p[0] + 0.5), math.frac(p[1] + 0.5) };
+        const sign = std.math.copysign(@as(f32, 1.0), n[2]);
+        return .{ math.frac(-sign * p[0] + 0.5), math.frac(-p[1] + 0.5) };
     }
 }
