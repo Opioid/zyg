b22a55bdb29e5519ced8a330ac7b086c130d8f1b
Opioid <benjamin.savs@gmail.com>
2025-06-07 14:51:55 -0700

    Alternative, more compact json notation for animation

--- src/util/animation_loader.zig
+++ src/util/animation_loader.zig
@@ -23,19 +23,27 @@ pub fn load(
     const fps = json.readFloatMember(value, "frames_per_second", 0.0);
     const frame_step = if (fps > 0.0) @as(u64, @intFromFloat(@round(@as(f64, @floatFromInt(Scene.UnitsPerSecond)) / fps))) else 0;
 
-    var iter = value.object.iterator();
-    while (iter.next()) |entry| {
-        if (std.mem.eql(u8, "keyframes", entry.key_ptr.*)) {
-            return loadKeyframes(
-                alloc,
-                entry.value_ptr.*,
-                default_trafo,
-                parent_trafo,
-                start_time,
-                frame_step,
-                graph,
-            );
-        }
+    if (value.object.get("keyframes")) |keyframes| {
+        return loadKeyframes(
+            alloc,
+            keyframes,
+            default_trafo,
+            parent_trafo,
+            start_time,
+            frame_step,
+            graph,
+        );
+    } else {
+        return loadTransformationsAndTimes(
+            alloc,
+            value.object.get("transformations"),
+            value.object.get("times"),
+            default_trafo,
+            parent_trafo,
+            start_time,
+            frame_step,
+            graph,
+        );
     }
 
     return Graph.Null;
@@ -80,4 +88,46 @@ pub fn loadKeyframes(
         },
         else => Graph.Null,
     };
+}
+
+pub fn loadTransformationsAndTimes(
+    alloc: Allocator,
+    trafos_value: ?std.json.Value,
+    times_value: ?std.json.Value,
+    default_trafo: Transformation,
+    parent_trafo: ?Transformation,
+    start_time: u64,
+    frame_step: u64,
+    graph: *Graph,
+) !u32 {
+    if (null == trafos_value) {
+        return Graph.Null;
+    }
+
+    const trafos = trafos_value.?.array.items;
+
+    const animation = try graph.createAnimation(alloc, @intCast(trafos.len));
+
+    var current_time = start_time;
+
+    for (trafos, 0..) |trafo, i| {
+        var keyframe = Keyframe{ .k = default_trafo, .time = current_time };
+
+        if (times_value) |times| {
+            if (i < times.array.items.len) {
+                keyframe.time = Scene.absoluteTime(json.readFloat(f64, times.array.items[i]));
+            }
+        }
+
+        json.readTransformation(trafo, &keyframe.k);
+        if (parent_trafo) |pt| {
+            keyframe.k = pt.transform(keyframe.k);
+        }
+
+        graph.animationSetFrame(animation, i, keyframe);
+
+        current_time += frame_step;
+    }
+
+    return animation;
 }
